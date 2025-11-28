e03ecb56143c08c8fc8ea29189c02fde57c0c1fb
Yash Yadav <yashyadav4651@gmail.com>
2025-11-28 20:01:39 +0530

    Add files via upload

--- Student_Hostel_Management.c
+++ Student_Hostel_Management.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 int choice;
+
 struct entry // structure For Entry
 {
     char name[50];
@@ -15,7 +16,7 @@ struct complaint // Structure for Complaint
 {
     char name[50];
     int room;
-    int phone;
+    long long phone;
     char complaint[200];
 };
 
@@ -31,13 +32,15 @@ void entry(int a)          // Function For Getting Entry
 
     printf("Room No. \n");
     scanf("%d", &en[a].room);
+    getchar();
 
     printf("Out Time: \n");
-    scanf("%s", &en[a].out_time);
+    fgets(en[a].out_time, sizeof(en[a].out_time), stdin);
     en[a].out_time[strcspn(en[a].out_time, "\n")] = '\0';
 
     printf("Purpose: \n");
-    scanf("%s", &en[a].purpose);
+    fgets(en[a].purpose, sizeof(en[a].purpose), stdin);
+    en[a].purpose[strcspn(en[a].purpose, "\n")] = '\0';
 
     printf("\n\n-----Entry Done----\n\n");
 }
@@ -75,32 +78,33 @@ void Complaint(int x) // Function For Taking Complaints
             com[i].name[strcspn(com[i].name, "\n")] = '\0';
             printf("Enter Your Room No.- \n");
             scanf("%d", &com[i].room);
+            getchar();
             printf("Enter Your Phone No:\n");
-            scanf("%d", &com[i].phone);
+            scanf("%lld", &com[i].phone);
             getchar();
             printf("Enter Your Complaint:\n");
             fgets(com[i].complaint, sizeof(com[i].complaint), stdin);
             com[i].complaint[strcspn(com[i].complaint, "\n")] = '\0';
-            break;
         }
+
         break;
     case 2:
         for (int i = 0; i < 100; i++)
         {
-            getchar();
+
             printf("\n------Enter Your Details------\n");
             printf("Enter Your Name:\n");
             fgets(com[i].name, sizeof(com[i].name), stdin);
             com[i].name[strcspn(com[i].name, "\n")] = '\0';
             printf("Enter Your Room No.- \n");
             scanf("%d", &com[i].room);
+            getchar();
             printf("Enter Your Phone No:\n");
-            scanf("%d", &com[i].phone);
+            scanf("%lld", &com[i].phone);
             getchar();
             printf("Enter Your Complaint:\n");
             fgets(com[i].complaint, sizeof(com[i].complaint), stdin);
             com[i].complaint[strcspn(com[i].complaint, "\n")] = '\0';
-            break;
         }
         break;
     case 3:
@@ -113,14 +117,13 @@ void Complaint(int x) // Function For Taking Complaints
             com[i].name[strcspn(com[i].name, "\n")] = '\0';
             printf("Enter Your Room No.- \n");
             scanf("%d", &com[i].room);
+            getchar();
             printf("Enter Your Phone No:\n");
-            scanf("%d", &com[i].phone);
+            scanf("%lld", &com[i].phone);
             getchar();
             printf("Enter Your Complaint:\n");
             fgets(com[i].complaint, sizeof(com[i].complaint), stdin);
             com[i].complaint[strcspn(com[i].complaint, "\n")] = '\0';
-
-            break;
         }
         break;
     case 4:
@@ -133,13 +136,13 @@ void Complaint(int x) // Function For Taking Complaints
             com[i].name[strcspn(com[i].name, "\n")] = '\0';
             printf("Enter Your Room No.- \n");
             scanf("%d", &com[i].room);
+            getchar();
             printf("Enter Your Phone No:\n");
-            scanf("%d", &com[i].phone);
+            scanf("%lld", &com[i].phone);
             getchar();
             printf("Enter Your Complaint:\n");
             fgets(com[i].complaint, sizeof(com[i].complaint), stdin);
             com[i].complaint[strcspn(com[i].complaint, "\n")] = '\0';
-            break;
         }
         break;
 
@@ -149,21 +152,21 @@ void Complaint(int x) // Function For Taking Complaints
 }
 int display_com(int v) // Display Complaint In-Case Anyone Want To Make Changes
 {
-    // int num;
-    // printf("Enter Your Room No.:-\n");
-    // scanf("%d", &num);
-
-    // for (int i = 0; i < 1; i++)
-    // {
-    //     if (num == com[i].room)
-    //     {
-    //         printf("-----Your Complaint-----\n");
-    //         printf("%s\n", com[i].name);
-    //         printf("%d\n", com[i].room);
-    //         printf("%d\n", com[i].phone);
-    //         printf("%s\n", com[i].complaint);
-    //     }
-    // }
+    int num;
+    printf("Enter Your Room No.:-\n");
+    scanf("%d", &num);
+    int i = 0;
+    for (; i < 100; i++)
+    {
+        if (num == com[i].room)
+        {
+            printf("-----Your Complaint-----\n");
+            printf("%s\n", com[i].name);
+            printf("%d\n", com[i].room);
+            printf("%lld\n", com[i].phone);
+            printf("%s\n", com[i].complaint);
+        }
+    }
     printf("\n----Your Complaint:---\n ");
 
     if (choice == 1)
@@ -182,18 +185,19 @@ int display_com(int v) // Display Complaint In-Case Anyone Want To Make Changes
     {
         printf("Department: Mess/Food\n");
     }
-    printf("Name: %s\n", com[v].name);
+    printf("Name: %s\n", com[i].name);
 
-    printf("Room No. %d\n", com[v].room);
+    printf("Room No. %d\n", com[i].room);
 
-    printf("Phone No.: %d\n", com[v].phone);
+    printf("Phone No.: %lld\n", com[i].phone);
 
-    printf("Purpose: %s\n", com[v].complaint);
+    printf("Complaint: %s\n", com[i].complaint);
+    return 0;
 }
 int main()
 {
-    int n, x;
-    char choice, choice_comp;
+    int n, x, entry_count = 0;
+    char choice1, choice_comp;
     char name[50];
 
     int arr[100];
@@ -211,34 +215,36 @@ start:
     switch (n)
     {
     case 1:
-        for (int i = 0; i < 100; i++)
+    {
+        int i = entry_count;
+    choice:
+
+        entry(i);
+        display_entry(i);
+
+    line:
+        printf("Do You Wish To Rewrite Your Entry?: Y for yes or N for no\n");
+        scanf(" %c", &choice1);
+        if (choice1 == 'Y' || choice1 == 'y')
         {
-        choice:
-
-            entry(i);
-            display_entry(i);
-            printf("Do You Wish To Rewrite Your Entry?: Y for yes or N for no\n");
-            scanf(" %c", &choice);
-            if (choice == 'Y' || choice == 'y')
-            {
-                goto choice;
-            }
-            else if (choice == 'N' || choice == 'n')
-            {
-                printf("\n\nYour Entry is Registered\n\n");
-                goto start;
-            }
-            else
-            {
-                printf("Enter a Valid Character\n");
-            }
-
-            break;
+            getchar(); // fix yes choice
+            goto choice;
         }
-
-        break;
+        else if (choice1 == 'N' || choice1 == 'n')
+        {
+            printf("\n\nYour Entry is Registered\n\n");
+            entry_count++;
+            goto start;
+        }
+        else
+        {
+            printf("Enter a Valid Character\n");
+            goto line;
+        }
+    }
+    break;
     case 2:
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 100; i++)
         {
 
             printf("Enter Your Name: \n");
@@ -249,7 +255,7 @@ start:
             scanf("%d", &x);
             getchar();
 
-            for (int j = 0; j < 100; j++)
+            for (int j = 0; j < entry_count; j++)
             {
                 if (x == en[j].room && strcmp(name, en[j].name) == 0)
 
@@ -267,7 +273,7 @@ start:
 
         break;
     case 3:
-        for (int j = 0; j < 1; j++)
+        for (int j = 0; j < 100; j++)
         {
         choice_comp:
             Complaint(j);
@@ -296,6 +302,9 @@ start:
         printf("\n\nExiting The Program.\n\nGood Bye!\n\n");
         return 0;
         break;
+    default:
+        printf("XX------Enter a Valid Choice------XX \n");
+        goto start;
     }
 
     return 0;
